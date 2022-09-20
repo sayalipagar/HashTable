4c3e3e030ca711b39bd30cc96e04f73ed0f8bbbf
sayalipagar <pagarsayali565@gmail.com>
2022-09-20 16:43:27 +0530

    [sayali] Add:FindFrequencyofParagraph

--- HashTable/MyMapNode.cs
+++ HashTable/MyMapNode.cs
@@ -49,8 +49,6 @@ namespace HashTable
                     }
                 }
             }
-            {
-            }
         }
     }
 }
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            MyMapNode<string, int> hash = new MyMapNode<string, int>(6);
-            string words = "to be or not to be";
+            MyMapNode<string, int> hash = new MyMapNode<string, int>(19);
+            string words = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
             string[] arr = words.Split(' ');
             LinkedList<string> checkForDuplication = new LinkedList<string>();
             foreach (string element in arr)
@@ -34,4 +34,4 @@
         }
     }
 }
-    
+           
